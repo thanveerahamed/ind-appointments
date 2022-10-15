99be07670fad30fef166bed813ae4cc5e099271d
Thanveer Ahamed <thanveer@ridedott.com>
2022-10-15 22:19:29 +0200

    fix: update the lottie files

--- src/App/App/ErrorBoundaryContent.tsx
+++ src/App/App/ErrorBoundaryContent.tsx
@@ -4,21 +4,12 @@ import { useState } from 'react';
 import { LoadingButton } from '@mui/lab';
 import Lottie from 'lottie-react';
 import errorAnimation from '../../assets/lottie/errorMessage.json';
+import loadingAnimation from '../../assets/lottie/loading.json';
 
 const ErrorBoundaryContent = () => {
   const [loading, setLoading] = useState<boolean>(false);
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Lottie
-          animationData={errorAnimation}
-          style={{
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '50%',
-          }}
-        />
-      </Grid>
       <Grid item xs={12}>
         <Box
           sx={{
@@ -29,18 +20,41 @@ const ErrorBoundaryContent = () => {
             textAlign: 'center',
           }}
         >
-          <Typography variant="h4">Oh crap!!! IND hates you! :) </Typography>
-          <LoadingButton
-            sx={{ marginTop: '10px' }}
-            variant="contained"
-            loading={loading}
-            onClick={() => {
-              setLoading(true);
-              window.location.reload();
-            }}
-          >
-            Try again.
-          </LoadingButton>
+          {loading ? (
+            <>
+              <Lottie
+                animationData={loadingAnimation}
+                style={{
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <Lottie
+                animationData={errorAnimation}
+                style={{
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+              />
+              <Typography variant="h4">
+                Oh crap!!! IND hates you! 😅
+              </Typography>
+              <LoadingButton
+                sx={{ marginTop: '10px' }}
+                variant="contained"
+                loading={loading}
+                onClick={() => {
+                  setLoading(true);
+                  setTimeout(() => window.location.reload(), 2000);
+                }}
+              >
+                Try again.
+              </LoadingButton>
+            </>
+          )}
         </Box>
       </Grid>
     </Grid>
