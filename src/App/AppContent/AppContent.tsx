bdf6ad699b251950833b346b6d3ce90d99eeeb94
Thanveer Ahamed <thanveer@ridedott.com>
2023-04-19 18:02:36 +0200

    fix: added log

--- src/App/AppContent/AppContent.tsx
+++ src/App/AppContent/AppContent.tsx
@@ -1,7 +1,12 @@
 import Box from "@mui/material/Box";
 import Home from "../../components/Home/Home";
+import {useEffect} from "react";
 
 const AppContent = () => {
+  useEffect(() => {
+    console.log(process.env)
+  })
+
   return <Box sx={{ margin: "10px" }}>
     <Home/>
   </Box>;
